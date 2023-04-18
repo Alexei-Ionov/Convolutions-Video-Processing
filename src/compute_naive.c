42c22860b70459c1bd9f3d5b76dbbe542558e669
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-18 15:11:47 -0700

    changes ?

--- src/compute_naive.c
+++ src/compute_naive.c
@@ -34,9 +34,8 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
     res[i] = dot(size_of_b, a_ptr, b_ptr);
     a_ptr += 1;
   }
-  (*output_matrix)->data = res;
-  (*output_matrix)->cols = size_diff + 1;
-  (*output_matrix)->rows = 1;
+  (*output_matrix) = res;
+
 
   return 0;
 }
