9918f8be65e2260c5a380dbdb4c821d0a2ea9e62
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-18 13:06:16 -0700

    changes to convolve

--- src/compute_naive.c
+++ src/compute_naive.c
@@ -13,17 +13,28 @@ int dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
 // Computes the convolution of two matrices
 int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   // TODO: convolve matrix a and matrix b, and store the resulting matrix in
-  int size_of_b = b_matrix->cols; //num rows == 1 
+  uint32_t size_of_b = b_matrix->cols; //num rows == 1 
   int end_ptr = size_of_b - 1;
   int start_ptr = 0;
+  int32_t *a_ptr = a_matrix->data;
+  int32_t *b_ptr = b_matrix->data;
+
   while (start_ptr != end_ptr) { 
-    int temp = b_matrix->data[end_ptr];
-    b_matrix->data[end_ptr] = b_matrix->data[start_ptr];
-    b_matrix->data[start_ptr] = temp;
+    uint32_t temp = b_ptr[end_ptr];
+    b_ptr[end_ptr] = b_ptr[start_ptr];
+    b_ptr[start_ptr] = temp;
     end_ptr -= 1;
     start_ptr += 1;
   }
-
+  uint32_t size_diff = size_of_b - a_matrix->cols;
+  int32_t *res = malloc(sizeof(uint32_t) * (size_diff + 1));
+  int i = 0;
+  for (; i <= size_diff; i++) { 
+    res[i] = dot(size_of_b, a_ptr, b_ptr);
+    a_ptr += 1;
+  }
+  (*output_matrix)->data = res;
+  
   return 0;
 }
 
