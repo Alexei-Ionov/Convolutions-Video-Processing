0ce892c75f8d025aa0cd66943d534404a0bc8c64
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-18 13:10:05 -0700

    testing

--- src/compute_naive.c
+++ src/compute_naive.c
@@ -18,6 +18,23 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   int start_ptr = 0;
   int32_t *a_ptr = a_matrix->data;
   int32_t *b_ptr = b_matrix->data;
+  while (start_ptr != end_ptr) { 
+    uint32_t temp = b_ptr[end_ptr];
+    b_ptr[end_ptr] = b_ptr[start_ptr];
+    b_ptr[start_ptr] = temp;
+    end_ptr -= 1;
+    start_ptr += 1;
+  }
+  uint32_t size_diff = size_of_b - a_matrix->cols;
+  int32_t *res = malloc(sizeof(uint32_t) * (size_diff + 1));
+  int i = 0;
+  for (; i <= size_diff; i++) { 
+    res[i] = dot(size_of_b, a_ptr, b_ptr);
+    a_ptr += 1;
+  }
+  (*output_matrix)->data = res;
+  (*output_matrix)->cols = size_diff + 1;
+  (*output_matrix)->rows = 1;
 
   return 0;
 }
