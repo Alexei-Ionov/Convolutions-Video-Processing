3bb2a384e69211b320a42d4b3d98f67042e201d8
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-19 14:25:54 -0700

    made a custom test

--- tools/custom_tests.py
+++ tools/custom_tests.py
@@ -4,3 +4,12 @@ from framework import Test, TestSpec, Task, Matrix
 @Test(seed=100)
 def test_example(test: TestSpec):
     test.add_task(Task(Matrix.random(100, 99), Matrix.random(10, 9)))
+
+
+@Test()
+def my_custom_test(test: TestSpec):
+    rows_a = 15
+    cols_a = 15
+    rows_b = 5
+    cols_b = 5
+    test.add_task(Task(Matrix.random(rows_a, cols_a), Matrix.random(rows_b, cols_b)))
