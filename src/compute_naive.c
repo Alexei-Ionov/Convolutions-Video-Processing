6f460c14c5e98e8906601b01766f471254c2110b
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-18 17:36:30 -0700

    2d convolve

--- src/compute_naive.c
+++ src/compute_naive.c
@@ -10,38 +10,75 @@ int dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
   }
   return res;
 }
+void flip_horizontal(int row, int num_col, int32_t *b) { 
+  int end_ptr = (row * num_col) + num_col - 1;
+  int start_ptr = (row * num_col);
+  int32_t temp;
+  while (start_ptr < end_ptr) { 
+    temp = b[end_ptr];
+    b[end_ptr] = b[start_ptr];
+    b[start_ptr] = temp;
+    end_ptr -= 1;
+    start_ptr += 1;
+  }
+}
+void flip_vertial(int num_rows; int num_col, int col, int32_t *b) {
+  int start_ptr = col;
+  int end_ptr = (num_rows * num_col) + col;
+  int32_t temp;
+  while (start_ptr < end_ptr) { 
+    temp = b[end_ptr];
+    b[end_ptr] = b[start_ptr];
+    b[start_ptr] = temp;
+    end_ptr -= num_col;
+    start_ptr += num_col;
+  }
+}
 
 // Computes the convolution of two matrices
 int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   // TODO: convolve matrix a and matrix b, and store the resulting matrix in
-  
-  uint32_t size_of_b = b_matrix->cols; //num rows == 1 
-  uint32_t size_of_a = a_matrix->cols;
 
-
-  int end_ptr = size_of_a - 1;
-  int start_ptr = 0;
+  uint32_t num_cols_b = b_matrix-> cols;
+  uint32_t num_cols_a = a_matrix-> cols;
+  uint32_t num_rows_a = a_matrix-> rows;
+  uint32_t num_rows_b = b_matrix-> rows;
+  int row = 0;
   int32_t *a_ptr = a_matrix->data;
   int32_t *b_ptr = b_matrix->data;
-  
-  while (start_ptr < end_ptr) { 
-    int32_t temp = a_ptr[end_ptr];
-    a_ptr[end_ptr] = a_ptr[start_ptr];
-    a_ptr[start_ptr] = temp;
-    end_ptr -= 1;
-    start_ptr += 1;
+  for (;row < num_rows_b; row++) { 
+    flip_horizontal(row, num_cols_b, b_ptr); 
   }
-  uint32_t size_diff = size_of_a - size_of_b;
-  int32_t *res = malloc(sizeof(int32_t) * (size_diff + 1));
+  int col = 0;
+  for (; col < num_cols_b; col++) { 
+    flip_vertial(num_rows_b, num_cols_b, col, b_ptr);
+  }
 
-  int i = 0;
-  for (; i <= size_diff; i++) { 
-    res[i] = dot(size_of_b, a_ptr, b_ptr);
-    a_ptr += 1;
+  uint32_t row_diff = num_rows_a - num_rows_b;
+  uint32_t col_diff = num_cols_a - num_cols_b;
+  int size_of_res = (col_diff + 1) * (row_diff + 1)
+  int32_t *res = malloc(sizeof(uint32_t) * (size_of_res));
+
+  row, col, index = 0;
+  int row_a, col_a = 0;
+  for (; index < size_of_res; index++) { 
+    int local = 0;
+    for (; col <= col_diff; col++) { 
+      for (; row < num_rows_b; row++) {
+        local += dot(num_cols_b, &(a_ptr[(row_a * num_cols_a) + col_a]), b_ptr);
+        row_a += 1;
+        b_ptr += num_cols_b;
+      }
+      b_ptr = b_matrix-> data;
+      col_a += 1;
+    }
+    col = 0;
+    col_a = 0;
+    res[index] = local;
+    row_a += 1;
   }
- 
   (*output_matrix)->data = res;
-
+  
   return 0;
 }
 
