82c6ea2e7e20ffb4a6da70045c22bbf6fcff8c00
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-19 15:34:05 -0700

    as

--- tools/create_tests.py
+++ tools/create_tests.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     tests = all_tests.keys()
     if len(sys.argv) > 1:
         tests = [t for t in all_tests if t == sys.argv[1]]
-    for test in tests['my_custom_test']:
-        print(f"Generating {test}")
-        all_tests[test].generate()
+    for test in tests:
+        if test == 'my_custom_test':
+            print(f"Generating {test}")
+            all_tests[test].generate()
