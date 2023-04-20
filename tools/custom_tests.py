e30d0c829d3452d4ab759a6fe68000c277d25729
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-19 17:24:54 -0700

    changes to custom test

--- tools/custom_tests.py
+++ tools/custom_tests.py
@@ -8,8 +8,8 @@ def test_example(test: TestSpec):
 
 @Test()
 def my_custom_test(test: TestSpec):
-    rows_a = 5
-    cols_a = 5
-    rows_b = 4
-    cols_b = 4
+    rows_a = 3
+    cols_a = 3
+    rows_b = 2
+    cols_b = 2
     test.add_task(Task(Matrix.random(rows_a, cols_a), Matrix.random(rows_b, cols_b)))
