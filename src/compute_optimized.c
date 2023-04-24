21556f0ffba57058265d08a27945b509e68eb202
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-24 16:25:09 -0700

    cahanges

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -144,15 +144,18 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
       #pragma omp parallel 
       { 
         #pragma omp for reduction(+:local,row_a2,b_ptr_index)
+        {
           local += dot(num_cols_b, &(a_ptr[(row_a2 * num_cols_a) + col]), &(b_ptr[b_ptr_index]));
           row_a2 += 1;
           b_ptr_index += num_cols_b;
+        }
+
       }
+      
     }
     res[index] = local;
     index += 1;
   }
-  
   output->data = res;
   output->cols = col_diff + 1;
   output->rows = row_diff + 1;
