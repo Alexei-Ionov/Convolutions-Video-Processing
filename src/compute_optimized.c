54fbde5e25057c2b7077c30e9422418b8d24d3c5
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-25 02:02:40 -0700

    tes

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -149,7 +149,8 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   print_matrix(b_ptr, num_rows_b, num_cols_b);
   transpose(num_rows_b, num_cols_b, b_ptr);
   printf("%s", "after: \n");
-  print_matrix(b_ptr, num_cols_b, num_rows_b);
+  print_matrix(b_ptr, num_rows_b, num_cols_b);
+  
 
 
   int col = 0;
