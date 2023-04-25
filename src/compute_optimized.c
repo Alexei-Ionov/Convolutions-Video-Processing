e462054ce576ebf02ef73b173acebaffad4fbff2
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-25 01:17:37 -0700

    tseting

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -134,7 +134,7 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
     flip(num_cols_b, &(b_ptr[col]));
   }
 
-  transpose(num_cols_b, num_rows_b, b_ptr);
+  //transpose(num_cols_b, num_rows_b, b_ptr);
   
   uint32_t row_diff = num_rows_a - num_rows_b;
   uint32_t col_diff = num_cols_a - num_cols_b;
