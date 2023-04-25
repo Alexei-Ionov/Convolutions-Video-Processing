9aa1558c82e285746120ea160f50d56e0baffe8a
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-24 23:52:17 -0700

    test

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -145,7 +145,7 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   for (int row = 0; row < num_rows_b; row++) { 
     flip_horizontal_naive(row, num_cols_b, b_ptr);
   }
-  for (int col = 0; col < num_cols_b, b_ptr) { 
+  for (int col = 0; col < num_cols_b; col++) { 
      flip_vertial(end_row, num_cols_b, col, b_ptr);
   }
 
