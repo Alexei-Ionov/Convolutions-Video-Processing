2eef83866e9e21e0e6a60240fa055ba436561fa6
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-24 23:54:37 -0700

    changes

--- src/compute_optimized_mpi.c
+++ src/compute_optimized_mpi.c
@@ -1,37 +1,36 @@
 #include <omp.h>
 #include <x86intrin.h>
-
+#include <immintrin.h>
 #include "compute.h"
-
 #define THRESHOLD 40
 #define OFFSET 8
-
+#define MAX(a,b) (((a)>(b))?(a):(b))
 // Computes the dot product of vec1 and vec2, both of size n
 int dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
-  
   // TODO: implement dot product of vec1 and vec2, both of size n
-  __m256 res = _mm256_set_ps(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-  u_int32_t cut_off = n - (n % 8);
+  __m256i res = _mm256_set_epi32 (0, 0, 0, 0, 0, 0, 0, 0);
+  uint32_t cut_off = n - (n % 8);
   int idx = 0;
-  __m256i vector1;
-  __m256i vector2;
-  while (cut_off) { 
-    vector1 = _mm256_load_si256 ((__m256i const *) vec1);
-    vector2 = _mm256_load_si256 ((__m256i const *) vec2);
-    __m256 vector3 = _mm256_mul_ps(vector1, vector2);
-    res = _mm256_add_ps(res, vector1);
-    vec1 += OFFSET;
-    vec2 += OFFSET;
-    cut_off -= OFFSET;
+  __m256i vector1, vector2;
+  uint32_t i = 0;
+  for (; i < cut_off; i += 8) { 
+    vector1 = _mm256_loadu_si256 ((__m256i const *) (vec1 + i));
+    vector2 = _mm256_loadu_si256 ((__m256i const *) (vec2 + i));
+    vector1 = _mm256_mullo_epi32 (vector1, vector2);
+    res = _mm256_add_epi32(res, vector1);
   }
-  int end = n % 8;
-  int j = 0;
-  int final = 0;
-  for (; j < end; j++) { 
+
+  uint32_t j = cut_off;
+  int32_t final = 0;
+  for (; j < n; j++) { 
     final += (vec1[j] * vec2[j]);
   }
-  return (int) (final + res[0] + res[1] + res[2] + res[3] + res[4] + res[5] + res[6] + res[7]);
+
+  int32_t temp[8];
+  _mm256_storeu_si256((__m256i *) temp, res);
+return (int) (final + temp[0] + temp[1] + temp[2] + temp[3] + temp[4] + temp[5] + temp[6] + temp[7]);
 }
+
 
 void flip_horizontal_naive(int row, int num_col, int32_t *b) { 
   int end_ptr = (row * num_col) + num_col - 1;
@@ -47,27 +46,56 @@ void flip_horizontal_naive(int row, int num_col, int32_t *b) {
 }
 
 
-void flip_horizontal_threaded(int n, int32_t *b_ptr) { 
-  // n == half size of the arrray
-  #pragma omp parallel 
-  {
-    int thread_num = omp_get_thread_num(); 
-    int num_threads = omp_get_num_threads();
-    int work = (n / num_threads);
-    int start = thread_num * work;
-    int finish = start + work;
-    if (finish > n) { 
-      finish = n;
-    }
-    for (; start < finish; start++) { 
-      int diff = n - start;
-      int temp = b_ptr[n + diff];
-      b_ptr[n + diff] = b_ptr[start];
-      b_ptr[start] = temp;
-    }
-  
+void flip_horizantal_optimized(uint32_t size, int32_t *row_ptr) { 
+  uint32_t half = size / 2;
+  #pragma omp parallel for 
+  for (uint32_t index = 0; index < half; index++) { 
+    int32_t temp = row_ptr[index];
+    row_ptr[index] = row_ptr[size - index - 1];
+    row_ptr[size - index - 1] = temp;
   }
 }
+
+  // n == half size of the arrray
+  // int furthest_completed = (n / 8) * 8;
+  // int end = 2 * n;
+  // for (int i = 0; i < end; i++) { 
+  //   printf("%d", row_ptr[i]);
+  //   printf("%s", " ");
+  // }
+  // printf("%s", "\n");
+
+  
+  // #pragma omp parallel 
+  // {
+  //   int thread_num = omp_get_thread_num(); 
+  //   int num_threads = omp_get_num_threads();
+  //   int work = (n / num_threads);
+    
+  //   int start = thread_num * work;
+  //   int finish = start + work;
+    
+  //   if (finish > n) { 
+  //     finish = n;
+  //   }
+  //   for (; start <= finish; start++) { 
+  //     int diff = n - start;
+  //     int temp = row_ptr[n + diff];
+  //     row_ptr[n + diff] = row_ptr[start];
+  //     row_ptr[start] = temp;
+  //   } 
+  // }
+  // for (; furthest_completed <= n; furthest_completed++) { 
+  //   int diff = n - furthest_completed;
+  //   int temp = row_ptr[n + diff];
+  //   row_ptr[n + diff] = row_ptr[furthest_completed];
+  //   row_ptr[furthest_completed] = temp;  
+  // }
+  // for (int i = 0; i < end; i++) { 
+  //   printf("%d", row_ptr[i]);
+  //   printf("%s", " ");
+  // }
+
 
 void flip_vertial(int row, int num_col, int col, int32_t *b) {
   int start_ptr = col;
@@ -100,24 +128,39 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
  
   // print_matrix(b_ptr, num_rows_b, num_cols_b);
 
-  int row = 0;
-  uint32_t half = num_cols_b / 2;
-
-  for (;row < num_rows_b; row++) { 
-    if (num_cols_b < THRESHOLD) { 
-      flip_horizontal_naive(row, num_cols_b, b_ptr); //overhead of starting threaded isn't worth so just do naive implementatino
-    } else { 
-      flip_horizontal_threaded(half, &(b_ptr[row * num_cols_b])); 
-    }
-  }
-  // print_matrix(b_ptr, num_rows_b, num_cols_b);
-
-  int col = 0;
+  
+  
+  // boost performance by multithreading
   int end_row = num_rows_b - 1;
-
-  for (; col < num_cols_b; col++) { 
-    flip_vertial(end_row, num_cols_b, col, b_ptr);
+  // for (int row = 0; row < num_rows_b; row++) { 
+  //   if (num_cols_b > 0) { 
+  //     flip_horizantal_optimized(num_cols_b, &(b_ptr[row]));
+  //   } else { 
+  //     flip_horizontal_naive(row, num_cols_b, b_ptr);
+  //   }
+  // }
+  // for (int col = 0; col < num_cols_b; col++) { 
+  //   flip_vertial(end_row, num_cols_b, col, b_ptr);
+  // }
+  for (int row = 0; row < num_rows_b; row++) { 
+    flip_horizontal_naive(row, num_cols_b, b_ptr);
   }
+  for (int col = 0; col < num_cols_b; col++) { 
+     flip_vertial(end_row, num_cols_b, col, b_ptr);
+  }
+
+  // #pragma omp parallel 
+  // {
+  //   #pragma omp for
+  //   for (int row = 0; row < num_rows_b; row++) { 
+  //     flip_horizantal_optimized(num_cols_b, &(b_ptr[row]));
+  //   }
+  //   #pragma omp barrier
+  //   #pragma omp for 
+  //   for (int col = 0; col < num_cols_b; col++) { 
+  //     flip_vertial(end_row, num_cols_b, col, b_ptr);
+  //   }
+  // }
   
   uint32_t row_diff = num_rows_a - num_rows_b;
   uint32_t col_diff = num_cols_a - num_cols_b;
@@ -125,33 +168,50 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   int32_t *res;
 
   res = malloc(sizeof(int32_t) * size_of_res);
-  
 
   uint32_t row_a = 0;
-  col = 0;
+  int col;
+  int row;
   int index = 0;
   int32_t local;
   int b_ptr_index;
 
+  // for (;row_a + num_rows_b <= num_rows_a; row_a++) { 
+  //   col = 0;
+  //   for (; col <= col_diff; col++) { 
+      
+  //     local = 0;
+  //     int row_a2 = row_a;
+  //     b_ptr_index = 0;
+  //     #pragma omp parallel 
+  //     { 
+  //       #pragma omp for reduction(+:local,row_a2,b_ptr_index)  
+  //         local += dot(num_cols_b, &(a_ptr[(row_a2 * num_cols_a) + col]), &(b_ptr[b_ptr_index]));
+  //         row_a2 += 1;
+  //         b_ptr_index += num_cols_b;
+  
+  //     }
+      
+  //   }
+  //   res[index] = local;
+  //   index += 1;
+  // }
   for (;row_a + num_rows_b <= num_rows_a; row_a++) { 
     col = 0;
     for (; col <= col_diff; col++) { 
-      
+      row = 0;
       local = 0;
       int row_a2 = row_a;
       b_ptr_index = 0;
-      #pragma omp parallel 
-      { 
-        #pragma omp for reduction(+:local,row_a2,b_ptr_index)
+      for (; row < num_rows_b; row++) {
         local += dot(num_cols_b, &(a_ptr[(row_a2 * num_cols_a) + col]), &(b_ptr[b_ptr_index]));
         row_a2 += 1;
         b_ptr_index += num_cols_b;
       }
+      res[index] = local;
+      index += 1;
     }
-    res[index] = local;
-    index += 1;
   }
-  
   output->data = res;
   output->cols = col_diff + 1;
   output->rows = row_diff + 1;
