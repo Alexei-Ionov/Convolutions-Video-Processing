0449ce919b611ff3e8eba861461f6838a808deae
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-25 01:51:23 -0700

    testing transpose

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -145,12 +145,12 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   // for (; row < num_rows_b; row++) { 
   //   flip(num_cols_b, &(b_ptr[row]));
   // }
-  print_matrix(b_ptr, num_rows_b, num_cols_b)
+  print_matrix(b_ptr, num_rows_b, num_cols_b);
   transpose(num_rows_b, num_cols_b, b_ptr);
-  print_matrix(b_ptr, num_cols_b, num_rows_b)
+  print_matrix(b_ptr, num_cols_b, num_rows_b);
 
 
-  // int col = 0;
+  int col = 0;
   // for (; col < num_cols_b; col++) { 
   //   flip(num_cols_b, &(b_ptr[col]));
   // }
