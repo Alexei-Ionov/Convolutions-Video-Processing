2e555210f5cc28dc03488286b9cfcc8d50b3d2ba
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-26 16:37:25 -0700

    testing

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -104,13 +104,14 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   int end_row = num_rows_b - 1;
  
   int row = 0;
+
   
   for (; row < num_rows_b; row++) { 
-    //flip_horizontal_naive(row, num_cols_b, b_ptr);
-    flip_horizantal_optimized(num_cols_b, &(b_ptr[row * num_cols_b]));
+    flip_horizontal_naive(row, num_cols_b, b_ptr);
+    //flip_horizantal_optimized(num_cols_b, &(b_ptr[row * num_cols_b]));
   }
   
-  if (num_cols_b > THRESHOLD) { 
+  if (num_rows_b > THRESHOLD) { 
     #pragma omp parallel 
     {
       #pragma omp for
