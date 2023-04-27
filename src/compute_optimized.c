5fe8e6d0ef2310a6fb16da96a4b9ae9d226d8929
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-27 02:22:49 -0700

    te

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -332,7 +332,7 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
     // printf("%s", "\n");
     print_matrix(temp, row_diff + 1, col_diff + 1);
     printf("%s", "\n");
-    print(row_diff);
+    printf("%d", row_diff);
     printf("%s", "\n");
     
   }
