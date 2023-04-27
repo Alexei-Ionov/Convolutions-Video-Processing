9935e8d5a7d76183d0614a8c6273b8a74292147b
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-27 02:59:39 -0700

    testing ag increasing

--- tools/custom_tests.py
+++ tools/custom_tests.py
@@ -9,9 +9,9 @@ def test_example(test: TestSpec):
 
 @Test()
 def my_custom_test(test: TestSpec):
-   for _ in range(50):
-        rows_a = randint(25, 500)
-        cols_a = randint(25, 500)
+   for i in range(2, 29):
+        rows_a = randint(math.floor(1.25**i), math.floor(1.25 ** (i + 1)))
+        cols_a = randint(math.floor(1.25**i), math.floor(1.25 ** (i + 1)))
         rows_b = randint(math.floor(rows_a / 4), math.floor(rows_a / 2))
         cols_b = randint(math.floor(cols_a / 4), math.floor(cols_a / 2))
         test.add_task(
