ae918c97d8ba3a3f44c4384833b602ccc0b7da6b
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-26 19:04:01 -0700

    testing

--- tools/custom_tests.py
+++ tools/custom_tests.py
@@ -11,6 +11,6 @@ def my_custom_test(test: TestSpec):
     rows_a = 50
     cols_a = 50
     rows_b = 12
-    cols_b = 33
+    cols_b = 1
 
     test.add_task(Task(Matrix.random(rows_a, cols_a, min_value=-2, max_value = 2), Matrix.random(rows_b, cols_b, min_value=0, max_value= 5)))
