dd2e15511db3a9bb497d7d7eaebbf802e233cbec
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-27 14:03:47 -0700

    testing

--- tools/custom_tests.py
+++ tools/custom_tests.py
@@ -13,8 +13,8 @@ def my_custom_test(test: TestSpec):
     for _ in range(50):
         rows_a = randint(25, 500)
         cols_a = randint(25, 500)
-        rows_b = randint(math.floor(rows_a / 4), math.floor(rows_a / 2))
-        cols_b = randint(math.floor(cols_a / 4), math.floor(cols_a / 2))
+        rows_b = rows_a - randint(1, 7)
+        cols_b = cols_a - randint(1, 7)
         test.add_task(
             Task(Matrix.random(rows_a, cols_a), Matrix.random(rows_b, cols_b))
         )
