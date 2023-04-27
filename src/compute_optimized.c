adf8543a13e95bd817bbec16d63f6c0c76a11c1a
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-26 18:53:10 -0700

    testing

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -98,7 +98,7 @@ void flip_horizantal_SIMD(int row, int num_cols, int32_t *row_ptr) {
     _mm256_storeu_si256 ((__m256i*) (row_ptr + start), end_vec);
     _mm256_storeu_si256 ((__m256i*) (row_ptr + end), start_vec);
     start += 8;
-    if (end - start < 16) { 
+    if (end - start < 15) { 
       end -= 1;
       break;
     }
