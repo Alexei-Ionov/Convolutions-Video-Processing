22d953a075aed190d473bb3e39a607abc34a02ab
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-26 17:59:29 -0700

    tcustomm test

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -78,6 +78,7 @@ int dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
   _mm256_storeu_si256((__m256i *) temp, res);
 return (int) (final + temp[0] + temp[1] + temp[2] + temp[3] + temp[4] + temp[5] + temp[6] + temp[7]);
 }
+
 void flip_horizantal_SIMD(int row, int num_cols, int32_t *row_ptr) { 
   int start = row * num_cols;
   int end = (row * num_cols) + num_cols - 8; //-8 for size of SIMD loads
@@ -136,11 +137,14 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   int end_row = num_rows_b - 1;
  
   int row = 0;
-
+  printf("%s", "before: \n")
+  print_matrix(b_ptr, num_rows_b, num_cols_b);
   for (; row < num_rows_b; row++) { 
     //flip_horizontal_naive(row, num_cols_b, b_ptr);
     flip_horizantal_SIMD(row, num_cols_b, b_ptr);
   }
+  printf("%s", "after: \n")
+  print_matrix(b_ptr, num_rows_b, num_cols_b);
   
   if (num_cols_b > THRESHOLD) { 
     #pragma omp parallel 
