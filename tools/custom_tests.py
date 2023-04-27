7ae45aee3ad6e3001c940e54641f30c8dc35b94b
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-27 00:33:36 -0700

    testing smaller matrices to see it more hits

--- tools/custom_tests.py
+++ tools/custom_tests.py
@@ -1,4 +1,4 @@
-from framework import Test, TestSpec, Task, Matrix
+from framework import Test, TestSpec, Task, Matrix, randint
 
 
 @Test(seed=100)
@@ -8,9 +8,20 @@ def test_example(test: TestSpec):
 
 @Test()
 def my_custom_test(test: TestSpec):
-    rows_a = 11
-    cols_a = 9
-    rows_b = 2
-    cols_b = 1
+    for _ in range(100):
+        rows_a = randint(15, 25)
+        cols_a = randint(15, 25)
+        rows_b = rows_a - randint(1, 7)
+        cols_b = cols_a - randint(1,7)
+        test.add_task(
+            Task(Matrix.random(rows_a, cols_a), Matrix.random(rows_b, cols_b))
+        )
 
-    test.add_task(Task(Matrix.random(rows_a, cols_a, min_value=-2, max_value = 2), Matrix.random(rows_b, cols_b, min_value=0, max_value= 5)))
+
+
+    # rows_a = 11
+    # cols_a = 9
+    # rows_b = 2
+    # cols_b = 1
+
+    # test.add_task(Task(Matrix.random(rows_a, cols_a, min_value=-2, max_value = 2), Matrix.random(rows_b, cols_b, min_value=0, max_value= 5)))
