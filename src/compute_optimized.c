6645a5ff32002c0b609b85d8e833baf6703a84b6
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-27 14:15:46 -0700

    t

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -229,8 +229,9 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   int32_t *res;
 
   res = malloc(sizeof(int32_t) * size_of_res);
-  
+  uint32_t cut_off = 0;
   if (row_diff >= 7) { 
+    cut_off = (row_diff + 1) - leftover + 1;
     #pragma omp parallel 
     {
       int thread_num = omp_get_thread_num();
@@ -240,11 +241,11 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
       uint32_t work = (row_diff + 1) / num_threads;           //might not divide perfectly so need to do manual work afterword
       uint32_t start = work * thread_num;
       uint32_t finish = start + work;
-      if (thread_num == num_threads - 1) { 
-        finish += 1;                      //accounts for hte last row to be completed :D
-      }
       if (finish > (row_diff + 1)) {
         finish = row_diff + 1;
+      }
+      if ((thread_num == num_threads - 1) && (finish <= row_diff + 1)) { 
+        finish += 1;                      //accounts for hte last row to be completed :D
       }
       
       for (; start < finish; start++) {
@@ -267,18 +268,17 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   } 
   uint32_t num_threads = 8;
   uint32_t leftover = (row_diff + 1) % num_threads;
-  uint32_t cut_off = (row_diff + 1) - leftover + 1;
   if (leftover != 0) {       //as long as there is even one row left to go... 
     #pragma omp parallel num_threads(leftover)
     {
       int thread_num = omp_get_thread_num();
       int num_threads = omp_get_num_threads();
       uint32_t start = thread_num + cut_off;
-      #pragma omp critical
-        printf("%d", row_diff + 1);
-        printf("%s", "   ");
-        printf("%d", num_threads);
-        printf("%s", "\n");
+      // #pragma omp critical
+      //   printf("%d", row_diff + 1);
+      //   printf("%s", "   ");
+      //   printf("%d", num_threads);
+      //   printf("%s", "\n");
       uint32_t finish = start + 1;
       
       if (finish > (row_diff + 1)) {
