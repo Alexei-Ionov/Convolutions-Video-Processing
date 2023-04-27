70f476ee35577d57b1472f9030211518c42136ac
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-27 00:35:23 -0700

    tes

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -198,10 +198,10 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   int32_t *res;
 
   res = malloc(sizeof(int32_t) * size_of_res);
-  print_matrix(a_ptr, num_rows_a, num_cols_a);
-  printf("%s", "\n");
-  print_matrix(b_ptr, num_rows_b, num_cols_b);
-  printf("%s", "\n");
+  // print_matrix(a_ptr, num_rows_a, num_cols_a);
+  // printf("%s", "\n");
+  // print_matrix(b_ptr, num_rows_b, num_cols_b);
+  // printf("%s", "\n");
   
     ///NOTE: im assuming that the size of res is AT LEAST 8.
   if (row_diff >= 7) { 
@@ -287,9 +287,9 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
       }
     }
   }
-  printf("%s", "res:");
-  printf("%s", "\n");
-  print_matrix(res, row_diff + 1, col_diff + 1);
+  // printf("%s", "res:");
+  // printf("%s", "\n");
+  // print_matrix(res, row_diff + 1, col_diff + 1);
 
 
   output->data = res;
