994ca2b05e3ae35d4d38c73a9625ce160fdecd91
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-26 19:28:32 -0700

    tesing non parallel flip vertial

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -183,35 +183,39 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
     }
     
   }
+  int col = 0; 
+  for (; col < num_cols_b; col++) { 
+    flip_vertial(end_row, num_cols_b, col, b_ptr);
+  }
   // printf("%s", "after: \n");
   // print_matrix(b_ptr, num_rows_b, num_cols_b);
 
   // test_flip(temp, b_ptr, size);
   
 
-  if (num_cols_b > THRESHOLD) { 
-    #pragma omp parallel 
-    {
-      #pragma omp for
-      for (int col = 0; col < num_cols_b; col++) { 
-        int end = ((num_rows_b - 1) * num_cols_b) + col;
-        int start = col;
-        while (start < end) { 
-          int32_t temp = b_ptr[end];
-          b_ptr[end] = b_ptr[start];
-          b_ptr[start] = temp;
-          start += num_cols_b;
-          end -= num_cols_b;
-        }
-      }
-    }
-  } else { 
-    int col = 0;
-    for (; col < num_cols_b; col++) { 
-      flip_vertial(end_row, num_cols_b, col, b_ptr);
-    }
+  // if (num_cols_b > THRESHOLD) { 
+  //   #pragma omp parallel 
+  //   {
+  //     #pragma omp for
+  //     for (int col = 0; col < num_cols_b; col++) { 
+  //       int end = ((num_rows_b - 1) * num_cols_b) + col;
+  //       int start = col;
+  //       while (start < end) { 
+  //         int32_t temp = b_ptr[end];
+  //         b_ptr[end] = b_ptr[start];
+  //         b_ptr[start] = temp;
+  //         start += num_cols_b;
+  //         end -= num_cols_b;
+  //       }
+  //     }
+  //   }
+  // } else { 
+  //   int col = 0;
+  //   for (; col < num_cols_b; col++) { 
+  //     flip_vertial(end_row, num_cols_b, col, b_ptr);
+  //   }
 
-  }
+  // }
   
   uint32_t row_diff = num_rows_a - num_rows_b;
   uint32_t col_diff = num_cols_a - num_cols_b;
