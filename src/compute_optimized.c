0eb01c4f6461cc760f90bd36f4af36f2b8721f19
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-26 19:49:08 -0700

    tes

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -235,11 +235,11 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
         #pragma omp parallel 
         { 
           #pragma omp for reduction(+:local,row_a2,b_ptr_index)
-          {
+      
           local += dot(num_cols_b, &(a_ptr[(row_a2 * num_cols_a) + col]), &(b_ptr[b_ptr_index]));
           row_a2 += 1;
           b_ptr_index += num_cols_b;
-          }
+          
         }
       } else { 
         int row = 0;
