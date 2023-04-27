6719f82624cd79e5fd1882c07f2c0d7d0764dea6
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-27 02:22:23 -0700

    testing

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -231,11 +231,11 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   int32_t *res;
 
   res = malloc(sizeof(int32_t) * size_of_res);
-  // int32_t* temp = malloc(sizeof(int32_t) * size_of_res);
-  // for (int h = 0; h < size_of_res; h++) {
-  //   temp[h] = res[h];
-  // }
-  // naive_solution(temp, a_ptr, b_ptr, num_rows_b, num_cols_a, num_cols_b, num_rows_a, col_diff);
+  int32_t* temp = malloc(sizeof(int32_t) * size_of_res);
+  for (int h = 0; h < size_of_res; h++) {
+    temp[h] = res[h];
+  }
+  naive_solution(temp, a_ptr, b_ptr, num_rows_b, num_cols_a, num_cols_b, num_rows_a, col_diff);
 
   // print_matrix(a_ptr, num_rows_a, num_cols_a);
   // printf("%s", "\n");
@@ -324,14 +324,18 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
     }
   }
 
-  // if (!check_solution(res, temp, size_of_res)) { 
-  //   printf("%s", "faulty res:");
-  //   printf("%s", "\n");
-  //   print_matrix(res, row_diff + 1, col_diff + 1);
-  //   printf("%s", "real res:");
-  //   printf("%s", "\n");
-  //   print_matrix(temp, row_diff + 1, col_diff + 1);
-  // }
+  if (check_solution(res, temp, size_of_res)) { 
+    // printf("%s", "faulty res:");
+    // printf("%s", "\n");
+    // print_matrix(res, row_diff + 1, col_diff + 1);
+    // printf("%s", "real res:");
+    // printf("%s", "\n");
+    print_matrix(temp, row_diff + 1, col_diff + 1);
+    printf("%s", "\n");
+    print(row_diff);
+    printf("%s", "\n");
+    
+  }
 
 
   output->data = res;
