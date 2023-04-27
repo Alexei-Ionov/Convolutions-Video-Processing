864791d554b6f06eb0ba34779cddd928d622d1d0
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-27 15:03:35 -0700

    testing

--- tools/custom_tests.py
+++ tools/custom_tests.py
@@ -10,7 +10,7 @@ def test_example(test: TestSpec):
 @Test()
 def my_custom_test(test: TestSpec):
    
-     for i in range(2, 29):
+     for i in range(28, 1, -1):
         rows_a = randint(math.floor(1.25**i), math.floor(1.25 ** (i + 1)))
         cols_a = randint(math.floor(1.25**i), math.floor(1.25 ** (i + 1)))
         rows_b = randint(math.floor(rows_a / 4), math.floor(rows_a / 2))
